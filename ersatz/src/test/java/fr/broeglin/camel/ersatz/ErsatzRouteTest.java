3c3f7730b29a4fd2c20fb862fab5bd322680b0ed
Dominique Broeglin <dominique.broeglin@gmail.com>
2014-05-06 23:13:23 +0200

    Added backend mock server and rsClient

--- ersatz/src/test/java/fr/broeglin/camel/ersatz/ErsatzRouteTest.java
+++ ersatz/src/test/java/fr/broeglin/camel/ersatz/ErsatzRouteTest.java
@@ -3,11 +3,20 @@ package fr.broeglin.camel.ersatz;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 import com.jayway.restassured.RestAssured;
 
 public class ErsatzRouteTest extends CamelBlueprintTestSupport {
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(8089);
 
 	@Override
 	protected String getBlueprintDescriptor() {
@@ -21,7 +30,26 @@ public class ErsatzRouteTest extends CamelBlueprintTestSupport {
 				.get("/svc/route")
 				.then()
 				.statusCode(200)
-				.body(equalTo("Hello true"));
+				.body(equalTo("Hello World"));
 	}
 
+	@Test
+	public void should_work1() throws Exception {
+		RestAssured.given().baseUri("http://localhost:8484")
+				.given()
+				.get("/svc/route")
+				.then()
+				.statusCode(200)
+				.body(equalTo("Hello World"));
+	}
+
+	@Before
+	public void stubBackend() {
+		stubFor(get(urlEqualTo("/backend"))
+				.withHeader("Accept", WireMock.equalTo("*/*"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", "text/plain")
+						.withBody("Hello World")));
+	}
 }
